555e94ad341a752450bc12d60b404a247c772c62
Cui Wei <cuiwei.exe@gmail.com>
2015-01-07 00:35:55 +0800

    add objective-c version

---
+++ objective-c/HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Cui Wei on 7/1/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+
+- (IBAction)handleMyButtonClick:(id)sender;
+@end
+
---
+++ objective-c/HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by Cui Wei on 7/1/15.
+//  Copyright (c) 2015 NUS CS3217. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)handleMyButtonClick:(id)sender {
+    
+    self.myLabel.text = @"Hello, World!";
+}
+@end
