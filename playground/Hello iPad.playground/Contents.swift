a772bcf30f3a3c5042d10cd4313ccf094d3809c8
Jinghan Wang <jinghan.w@icloud.com>
2016-12-17 23:41:46 +0800

    update to Swift 3

--- playground/Hello iPad.playground/Contents.swift
+++ playground/Hello iPad.playground/Contents.swift
@@ -1,17 +1,17 @@
 import UIKit
-import XCPlayground
+import PlaygroundSupport
 
 /*:
 ## Make a "Hello iPad" label
 ----
 * First, we create a UILabel with a specific frame
 */
-var myLabel = UILabel(frame: CGRectMake(0, 0, 140, 40))
+var myLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 140, height: 40))
 
 /*:
 * Then we change the background color of the label to light gray
 */
-myLabel.backgroundColor = UIColor.lightGrayColor()
+myLabel.backgroundColor = UIColor.lightGray
 /*:
 * By clicking the **Quick Look** (a.k.a. **Show Result**) icon on the right, you can see what the label currently looks like
 ![Instruction Image](image1.png)
@@ -22,8 +22,8 @@ myLabel.text = "Hello iPad"
 
 //:* We can also change the attributes such as **font**, **text color**, **text alignment**, etc.
 myLabel.font = UIFont(name: "Helvetica", size: 20)
-myLabel.textColor = UIColor.redColor()
-myLabel.textAlignment = NSTextAlignment.Center
+myLabel.textColor = UIColor.red
+myLabel.textAlignment = NSTextAlignment.center
 
 /*:
 * Moreover, we can add rounded corners
@@ -41,12 +41,13 @@ myLabel
 ----
 * Create a UIView with specific frame
 */
-let view = UIView(frame: CGRectMake(0, 0, 200, 100))
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
 
 /*:
 * Set the view to liveView, so we can see the view in Timeline
 */
-XCPlaygroundPage.currentPage.liveView = view
+PlaygroundPage.current.liveView = view
+
 /*:
 * To display the Timeline, click on the Assistant Editor button
 > either ![Navigation Bar](image2.png)
