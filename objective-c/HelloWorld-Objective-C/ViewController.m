4f88237890e01fad99ebbbdf7066d554da0594f3
Louis Lai <hoangdung2506@gmail.com>
2017-12-28 15:04:34 +0700

    Convert tabs to spaces & Fix broken storyboard outlet

--- objective-c/HelloWorld-Objective-C/ViewController.m
+++ objective-c/HelloWorld-Objective-C/ViewController.m
@@ -18,18 +18,19 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 
 - (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 
 - (IBAction)myButtonDidClick:(id)sender {
-	self.myLabel.text = @"Hello, World!";
+    self.myLabel.text = @"Hello, World!";
 }
 @end
+
--- swift/HelloWorld/ViewController.swift
+++ swift/HelloWorld/ViewController.swift
@@ -10,22 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	@IBOutlet weak var myLabel: UILabel!
-	
-	@IBAction func myButtonDidClick(_ sender: Any) {
-		myLabel.text = "Hello, World!"
-	}
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
+    @IBOutlet weak var myLabel: UILabel!
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+    @IBAction func myButtonDidClick(_ sender: Any) {
+        myLabel.text = "Hello, World!"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 
 }
-
